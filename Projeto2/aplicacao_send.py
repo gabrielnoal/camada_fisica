6ccf699079c6fcde21006eb46e3cd15ff7f7b26c
Gabriel Oliva <gabriel.oliva@hotmail.com>
2018-08-15 17:05:01 -0300

    Projeto 2 em andamento

---
+++ Projeto2/aplicacao_send.py
@@ -0,0 +1,69 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação
+####################################################
+
+print("comecou")
+
+from enlace import *
+import time
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer a
+# comunicaçao
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+#serialName = "COM5"                  # Windows(variacao de)
+
+
+
+print("porta COM aberta com sucesso")
+
+
+
+def main():
+    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+    #verificar que a comunicação foi aberta
+    print("comunicação aberta")
+
+
+    # Gera dados para Transmissao
+    print ("gerando dados para transmissao :")
+
+
+    txBuffer = open("picture.jpg", "rb").read()
+    txLen    = len(txBuffer)
+    print(txLen)
+
+    # Transmite dado
+    print("tentado transmitir .... {} bytes".format(txLen))
+    com.sendData(txBuffer)
+
+
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
